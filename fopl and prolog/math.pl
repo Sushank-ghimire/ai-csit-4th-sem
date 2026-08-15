c7c58370416a65bf53d0487d6cd9dc8da47e5058
Sushank-ghimire <ghimiresushank08@gmail.com>
2026-08-15 18:25:30 +0545

    Add mathematical operations: HCF, LCM, cube, sum, difference, and division

---
+++ fopl and prolog/math.pl
@@ -0,0 +1,59 @@
+% HCF (Highest Common Factor) usong Euclidean algorithm
+
+hcf(A, B, H) :-
+  A > B,
+  R is B mod A,
+  hcf(B, R, H).
+
+hcf(A, B, H) :-
+  A < B,
+  R is B mod A,
+  hcf(A, R, H).
+
+hcf(A, B, A) :-
+  A =:= B.
+
+% LCM (Least Common Multiple)
+
+lcm(A, B, L) :-
+  hcf(A, B, H),
+  L is (A * B) // H.
+
+% Cube
+cube :-
+  write('Enter a number: '),
+  read(N),
+  C is N * N * N,
+  write('Cube of the number is: '),
+  write(C).
+
+% Sum
+sum :-
+  write('Enter first number: '),
+  read(A),
+  write('Enter second number: '),
+  read(B),
+  S is A + B,
+  write('Sum is: '),
+  write(S).
+
+% Difference
+difference :-
+  write('Enter first number: '),
+  read(A),
+  write('Enter second number: '),
+  read(B),
+  D is A - B,
+  write('Difference is: '),
+  write(D).
+
+
+% Difference
+divide :-
+  write('Enter first number: '),
+  read(A),
+  write('Enter second number: '),
+  read(B),
+  D is A / B,
+  write('Division of first/second is: '),
+  write(D).
